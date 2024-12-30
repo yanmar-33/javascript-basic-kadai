a0f9182fc1684c9d7b61e17acfc65da9e444d871
yanmar-33 <water.resistant93@gmail.com>
2024-12-30 17:28:13 +0900

    kadai_mdn_js

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+const year = today.getUTCFullYear();
+
+const month = today.getUTCMonth();
+
+const dayOfMonth = today.getUTCDate();
+
+console.log( year + '年' + month + '月' + dayOfMonth + '日');
