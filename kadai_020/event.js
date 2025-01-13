5664ceff5fd5c3b22fd00735ce7cb7f4432596e5
山﨑　博史 <>
2025-01-14 06:12:48 +0900

    修正完了

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,2 +1,20 @@
-const btn = document.getElementById('text')
+//btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.querySelector("#btn");
+
+//textというid遠持つHTML要素を取得し、定数に代入する
+const text = document.querySelector("#text");
+console.log(text);
+
+//HTML要素がクリックされた時にイベントを処理する
+btn.addEventListener('click', () =>{
+
+
+    //textの文字を編集する
+    text.textContent = 'ボタンがクリックされました。';
+    
+    
+    });
+
+
+
 
